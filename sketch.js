77ae44bb7a3ae6ba7f025782b899f1126ef5cbb4
Caroline Andrade <andradecostacaroline@gmail.com>
2023-05-10 21:57:45 -0300

    enviando projeto completo

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarros();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+  
+}
